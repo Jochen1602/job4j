80800e6cc8a0ce1c3491c7b33f9182c574fc3bae
Jochen1602 <jochen13@yandex.ru>
2018-10-09 14:13:09 +0300

    Update 1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
+/***
+*Class Calculate решение задачи 001 урок 1
+*@author antontokarev
+*@since 09.10.2018
+***/
 
 public class Calculate{
+
+/***
+*Метод, выводящий в консоль Hello world
+*@param arg - arg
+***/
 public static void main(String[] args) {
 	System.out.println("Hello world");
 }
