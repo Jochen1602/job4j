8f5bfd19b17b844347cec871202f9851bec29637
Jochen1602 <jochen13@yandex.ru>
2018-11-02 13:51:20 +0300

    upd Кофемашина

--- chapter_002/src/main/java/ru/job4j/coffee/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/coffee/CoffeeMachine.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
  *@since 01.11.2018
  */
 public class CoffeeMachine {
+    private final int[] coins = new int[]{10, 5, 2, 1};
     /**
      * Метод, определяющий какую сдачу нужно выдать монетами по 10, 5, 2 и 1
      * @param value купюра
@@ -15,23 +16,14 @@ public class CoffeeMachine {
      * @return массив из монет в убывающем порядке
      */
     public int[] changes(int value, int price) {
-        int monets = value - price;
-        int[] temp = new int[monets / 10 + 3];
+        int[] temp = new int[(value - price) / 10 + 3];
         int counter = 0;
-        for (int i = 0; i < monets / 10; i++) {
-            temp[counter++] = 10;
-        }
-        monets -= (monets / 10) * 10;
-        for (int i = 0; i < monets / 5; i++) {
-            temp[counter++] = 5;
-        }
-        monets -= (monets / 5) * 5;
-        for (int i = 0; i < monets / 2; i++) {
-            temp[counter++] = 2;
-        }
-        monets -= (monets / 2) * 2;
-        if (monets == 1) {
-            temp[counter++] = 1;
+        value -= price;
+        for (int coin : coins) {
+            while (value - coin >= 0) {
+                temp[counter++] = coin;
+                value -= coin;
+            }
         }
         return Arrays.copyOf(temp, counter);
     }
