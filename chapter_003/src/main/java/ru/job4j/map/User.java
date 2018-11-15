fd1028bd8d01143b23678be8e8238e280e3943bd
Jochen1602 <jochen13@yandex.ru>
2018-11-15 17:06:45 +0300

    1. Создать модель User[#84121]

---
+++ chapter_003/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,14 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ *class User Решение задачи 1. Создать модель User[#84121]
+ *@author antontokarev
+ *@since 15.11.2018
+ */
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+}
