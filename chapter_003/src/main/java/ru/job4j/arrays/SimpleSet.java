1ac3b0262aba45ffdfa8b38072717da7465b8d09
Jochen1602 <jochen13@yandex.ru>
2018-11-21 14:02:23 +0300

    upd 1. Реализовать коллекцию Set на массиве [ #84118]

--- chapter_003/src/main/java/ru/job4j/arrays/SimpleSet.java
+++ chapter_003/src/main/java/ru/job4j/arrays/SimpleSet.java
@@ -10,18 +10,31 @@ import java.util.Iterator;
 public class SimpleSet<E> implements Iterable<E> {
     private DynamicList<E> values = new DynamicList<>();
 
+    /**
+     * Метод добавления элемета в множество.
+     * @param e что добавляем.
+     */
     public void add(E e) {
-        boolean check = true;
+        if (isYet(e) == -1) {
+            this.values.add(e);
+        }
+    }
+
+    /**
+     * Метод проверяет, есть ли уже в множестве такой элемент.
+     * @param e элемент на проверку.
+     * @return -1 если его там нет, иначе номер ячейки, в которой он находится.
+     */
+    public int isYet(E e) {
+        int result = -1;
         for (int i = 0; i < this.values.getSize(); i++) {
             if (this.values.get(i) != null) {
                 if (this.values.get(i).equals(e)) {
-                    check = false;
+                    result = i;
                 }
             }
         }
-        if (check) {
-            this.values.add(e);
-        }
+        return result;
     }
 
     @Override
