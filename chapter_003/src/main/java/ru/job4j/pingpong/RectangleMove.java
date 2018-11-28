6962d4d9fd84d320e83edeef1a83e903e064dc78
Jochen1602 <jochen13@yandex.ru>
2018-11-28 15:36:44 +0300

    2. Добавить метод boolean isBinary()[#84135]

---
+++ chapter_003/src/main/java/ru/job4j/pingpong/RectangleMove.java
@@ -0,0 +1,32 @@
+package ru.job4j.pingpong;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        int dx = 5 + (int) (Math.random() * 5);
+        int dy = 5 + (int) (Math.random() * 5);
+        while (true) {
+            this.rect.setX(this.rect.getX() + dx);
+            this.rect.setY(this.rect.getY() + dy);
+            if (this.rect.getX() <= 2 || this.rect.getX() >= 298) {
+                dx *= -1;
+            }
+            if (this.rect.getY() <= 2 || this.rect.getY() >= 298) {
+                dy *= -1;
+            }
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
